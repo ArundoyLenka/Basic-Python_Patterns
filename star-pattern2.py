39e5e63a0bbf481fa3fbe31d0bdfff489b2b7674
ArundoyLenka <83690729+ArundoyLenka@users.noreply.github.com>
2022-03-24 17:32:49 +0530

    Delete star-pattern2.py

--- star-pattern2.py
+++
@@ -1,9 +0,0 @@
-n = 5
-for i in range(n):
-    for j in range(i, n):           #so basically what we are doing is printing blank spaces in this loop
-        print(' ', end='')          #and at the end of the blank spaces we are printing a "*"
-    
-    for j in range(i+1):
-        print('*', end='')
-
-    print()
